bdc7b0542e622936576c6a0b6d747cc5f83e86b7
Tobe O <thosakwe@gmail.com>
2018-03-17 10:01:48 -0400

    Gulp-based build

---
+++ src/scenes/boot.ts
@@ -0,0 +1,33 @@
+define(['scenes/dialogue'], function (DialogueState) {
+  return class BootScene extends DialogueState {
+
+    preload() {
+      const game = this.game;
+      game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+      game.scale.refresh();
+      window.onresize = game.scale.refresh.bind(game.scale);
+
+      this.load.audio('sounds.title', 'assets/sounds/title.mp3');
+      this.load.image('images.title', 'assets/images/title.png');
+    }
+
+    create() {
+      const titleMp3 = this.add.sound('sounds.title');
+      titleMp3.play('', 0, 1, true);
+
+      this.add.sprite(0, 0, 'images.title');
+
+      const text = this.add.text(115, 450, 'PRESS SPACE', {
+        fill: '#fff'
+      });
+
+      const timer = this.time.create();
+      timer.loop(500, function () {
+        if (text.fill === '#fff')
+          text.fill = 'transparent';
+        else text.fill = '#fff';
+      });
+      timer.start();
+    }
+  };
+});
---
+++ src/scenes/dialogue.ts
@@ -0,0 +1,7 @@
+define(function() {
+  return class DialogueState extends Phaser.State {
+    reee() {
+
+    }
+  }
+});
